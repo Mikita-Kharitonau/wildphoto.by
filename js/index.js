5014ab63c37cec5b51d65a71ceafc03afdfef33a
ppomaker <khari-vitalij99@mail.ru>
2019-02-26 19:20:01 +0300

    flex header

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar__items')
+
+    navs.forEach(nav => nav.classList.toggle('navbar__toggleShow'));
+}
+
+document.querySelector('.navbar__toggle')
+    .addEventListener('click', classToggle);
